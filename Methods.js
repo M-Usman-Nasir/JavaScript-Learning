80f559abfa6e6c2bf7aa8ce1182781a09489cc4e
Usman Nasir <157220338+M-Usman-Nasir@users.noreply.github.com>
2024-06-06 04:22:42 +0000

    Class work doing

---
+++ Methods.js
@@ -0,0 +1,16 @@
+// Numbers and Object Methods
+
+const numbers = 10;
+console.log (typeof numbers);
+
+const numToString = number.numToString();
+
+const result = 160.3695877;
+
+const fixedValue = result.toFixed (2);
+
+console.log (fixedValue);
+
+const preciseValue = result.toPrecision (6);
+
+console.log (preciseValue);
