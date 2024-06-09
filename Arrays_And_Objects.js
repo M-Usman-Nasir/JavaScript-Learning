eff8ab9a8886b4301625b8dc1cb7b556579dea45
Usman Nasir <157220338+M-Usman-Nasir@users.noreply.github.com>
2024-06-09 16:20:38 +0000

    Practicing

--- Arrays_And_Objects.js
+++ Arrays_And_Objects.js
@@ -2,80 +2,81 @@
 // 1. Always use pluralization
 // 2. array literals "[]"
 
-// Add and remove Array Element
-// Practice No.: 01
+// Practice No.: 01 (Loop over the Array)
 // const colors = ["red", "green", "yellow", "blue", "pink"];
 // for (let i = 0; i < colors.length; i++) {
     //     console.log (colors [i]);
     // }
-    // const names = "Ali";
-    // console.log (colors.includes ("red"));
     
-    // console.log (Array.isArray (names));
-    // console.log (Array.isArray (colors));
-    // colors [1] = "White"; // to modify colors
+// Practice No.: 02 (Common Array Methods)
+// const names = "Ali";
+// console.log (colors.includes ("red"));
     
-    // console.log (colors);
-    // console.log (colors.length);
-    // console.log (colors [0]);
+// console.log (Array.isArray (names));
+// console.log (Array.isArray (colors));
+// colors [1] = "White"; // to modify colors
     
-    // colors.push ("black");
-    // colors.unshift ("white");
-    // colors.pop ();
-    // colors.shift (); // remove the red
-    // console.log (colors);
+// const colorSetOne = ["red", "blue"]
+// const colorSetTwo = ["green", "white"]
     
-    // const colorSetOne = ["red", "blue"]
-    // const colorSetTwo = ["green", "white"]
-    
-    // const combinedArray = colorSetOne.concat (colorSetTwo);
-    // console.log (combinedArray);
-    
-    // array literals
-    // Practice No.: 02
-    // const colors = ['red', 'blue', 'green', 'pink']
-    // for (let i = 0; i < colors.length; i++) {
-    //   console.log(colors[i])
-    // }
-    
-    // const name1 = 'John'
-    // console.log(colors.includes('orange'))
-    
-    // console.log(Array.isArray(name1))
-    
-    // Change / Modify Array Element 
-    // colors[1] = 'white'
-    // colors[3] = 'black'
-    // colors[10] = 'purple'
-    
-    // console.log(colors)
-    // console.log(colors.length)
-    
-    // console.log(colors[1])
-    
-    // colors.push('purple')
-    // console.log(colors)
-    
-    // colors.unshift('white')
-    // console.log(colors)
-    
-    // colors.pop()
-    // console.log(colors)
-    
-    // colors.shift()
-    // console.log(colors)
+// const combinedArray = colorSetOne.concat (colorSetTwo);
+// console.log (combinedArray);
 
-// 
-    // const nums = [13, 9, 15, 10, 12];
-    // let sum = 0;
-    // for (let i = 0; i < nums.length; i++) {
-        //     sum = sum + nums[i]
-        // }
+// console.log (colors);
+// console.log (colors.length);
+// console.log (colors [0]);
+    
+// Add and remove Array Element
+// colors.push ("black");
+// colors.unshift ("white");
+// colors.pop ();
+// colors.shift (); // remove the red
+// console.log (colors);
+    
+// Array literals
+// Practice No.: 03
+// const colors = ['red', 'blue', 'green', 'pink']
+// for (let i = 0; i < colors.length; i++) {
+//   console.log(colors[i]) }
+        
+// const name1 = 'John'
+// console.log(colors.includes('orange'))
+        
+// console.log(Array.isArray(name1))
+        
+// Change / Modify Array Element 
+// colors[1] = 'white'
+// colors[3] = 'black'
+// colors[10] = 'purple'
+        
+// console.log(colors)
+// console.log(colors.length)
+        
+// console.log(colors[1])
+        
+// colors.push('purple')
+// console.log(colors)
+        
+// colors.unshift('white')
+// console.log(colors)
+    
+// colors.pop()
+// console.log(colors)
+    
+// colors.shift()
+// console.log(colors)
+
+// JavaScript Array Exercises 
+// Question No. 1 and Answer 
+// const nums = [13, 9, 15, 10, 12];
+// let sum = 0;
+// for (let i = 0; i < nums.length; i++) {
+//     sum = sum + nums[i]}
 // console.log("sum" , sum);
 
+// Question No. 2 and Answer 
 // const nums = [13, 9, 15, 10, 15, 80];
 // let largestNum = 0;
-
 // for (let i = 0; i < nums.length; i++) {
 //     if (nums[i] > largestNum) {
 //         largestNum = nums[i];
@@ -83,7 +84,8 @@
 // }
 // console.log("largestNum", largestNum);
 
-// const products = ["keyboard", "mouse", "keyboard", "speaker", "printer", "headphone", "printer", "keyboard"]
+// Question No. 3 and Answer
+// const products = ["keyboard", "mouse", "keyboard", "speaker", "printer", "headphone", "printer", "keyboard", "mouse"]
 // const uniqueProducts = [];
 // for (let i = 0; i < products.length; i++) {
 //     if (!uniqueProducts.includes(products[i])) {
@@ -92,60 +94,52 @@
 // }
 // console.log(uniqueProducts);
 
-
+// Practice No. 1
 // const products = ["keyboard", "mouse", "keyboard", "speaker", "printer", "headphone", "printer", "keyboard"]
-
 // console.log (products);
-
 // const slice = products.slice(0, 1);
 // console.log("products" , products);
 // console.log ("slice", slice);
-
-
 // const products = ["keyboard", "mouse", "keyboard", "speaker", "printer", "headphone", "printer", "keyboard"]
 
+// Practice No. 2
 // const splice = products.splice (2, 2);
 // console.log("slice", splice);
 
+// Extras No. 1 and 2
 // let x = 10
 // let y = 20
-
 // let z = x
-// x = y =z
-
+// x = y = z
 // console.log(+ "23");
 // console.log("x",x);
 // console.log("y", y);
 
-
+//  Extras No. 3
 // const firstName = "Ali"
 // const lastName = "Taha"
-
 // const fullName = firstName + " " + lastName
 // console.log (fullName);
 
-
 // let x = 10;
 // let y = 20;
-
 // console.log (x === y);
 
 // let x = [10];
 // let y = [20];
-
 // console.log (x === y);
 
 //  Objects
-
+// Exercise No. 1
 // const product = {
-//     name1: "mouse",
+//     name: "mouse",
 //     description: "gaming mouse",
 //     colors: ["red", "blue"],
 //     price : 100,
 //     inStock: true,
 //     units : 10,
 // }
-// console.log(product);
+// console.log (product);
 
 // const objectDetails = {
 //     name2: "Book",
@@ -162,6 +156,7 @@
 // console.log(objectDetails.author);
 // console.log(objectDetails ["name2"]);
 
+// Object Exercise No. 2
 // const restaurant = {
 //     name1: "Delhi",
 //     address: `$ {Math.floor(Math.random() * 100 ) + 1} Jinnah Ave`,
@@ -172,7 +167,6 @@
 // }
 
 // console.log (restaurant); //
-
 
 //     name2: "Taha",
 //     age: 32,
