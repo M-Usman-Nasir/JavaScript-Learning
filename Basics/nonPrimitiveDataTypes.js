283ae01008932dbdd302510a0c5b089ec5ee2b8b
Mohammad Usman Nasir <usmannasir98@gmail.com>
2024-06-26 18:45:16 +0500

    Learning

--- Basics/nonPrimitiveDataTypes.js
+++ Basics/nonPrimitiveDataTypes.js
@@ -4,10 +4,34 @@
 // 3. Function
 
 // _____ Array _________
-let fruits = ["apple", "banana", "orange", "grapes", "strawberry"] 
-let x = [1, 2, "Ali"]
-let myVariable = [1, 'Bob', 'Steve', 10];
-fruits [0]
-fruits [3]
-myVariable[0]
-myVariable[1]
+// let fruits = ["apple", "banana", "orange", "grapes", "strawberry"]; 
+// let x = [1, 2, "Ali"];
+// let myVariable = [1, 'Bob', 'Steve', 10];
+
+// console.log (fruits);
+// console.log (x);
+// console.log (myVariable);
+
+// _______ Object ___________
+// const car = {
+//     type: "Toyota",		
+//     model: "Corolla",  		
+//     year: 2009};
+
+// console.log (car);
+
+// const person = { 
+//     name: "Alice",	
+//     age: 30,		  
+//     hobbies: ["reading", "coding"]};
+// console.log (person);
+// console.log (person.name);
+// console.log (person.hobbies[1]);
+
+// _________ Functions ___________
+function greet (name) {
+    return "Hello, " + name + "!";
+  }
+  
+const message = greet("Bob");
+ console.log(message); // Outputs "Hello, Bob!"
