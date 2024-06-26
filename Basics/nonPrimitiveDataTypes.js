e2eb92e0546f26c10550e06ea1aec8900bb87990
Mohammad Usman Nasir <usmannasir98@gmail.com>
2024-06-26 15:45:55 +0500

    prac

--- Basics/nonPrimitiveDataTypes.js
+++ Basics/nonPrimitiveDataTypes.js
@@ -2,3 +2,12 @@
 // 1. Array
 // 2. Object 
 // 3. Function
+
+// _____ Array _________
+let fruits = ["apple", "banana", "orange", "grapes", "strawberry"] 
+let x = [1, 2, "Ali"]
+let myVariable = [1, 'Bob', 'Steve', 10];
+fruits [0]
+fruits [3]
+myVariable[0]
+myVariable[1]
