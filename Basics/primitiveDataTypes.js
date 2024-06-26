730f8a9ae6699366bbe471d955432c3d56968820
Mohammad Usman Nasir <usmannasir98@gmail.com>
2024-06-26 15:17:30 +0500

    Made Folders

---
+++ Basics/primitiveDataTypes.js
@@ -0,0 +1,33 @@
+// 
+
+let firstName = "Ali";
+let unassigned;
+let isMarried = true;
+let score = 25;			
+let ab = 123;			
+let b = 123.456;
+let empty = null;
+let abc = 123n;		
+let bigInt = BigInt(Number.MAX_SAFE_INTEGER) + 2n;
+const mySymbol = Symbol();
+
+console.log(firstName);
+console.log(typeof firstName); 
+console.log(unassigned);
+console.log(typeof unassigned); 
+console.log(isMarried);
+console.log(typeof isMarried); 
+console.log(score);
+console.log(typeof score); 
+console.log(ab);
+console.log(typeof ab); 
+console.log(b);
+console.log(typeof b); 
+console.log(empty);
+console.log(typeof empty); 
+console.log(abc);
+console.log(typeof abc); 
+console.log(bigInt);
+console.log(typeof bigInt); 
+console.log(mySymbol);
+console.log(typeof mySymbol);
---
+++ Basics/var_let_const.js
@@ -0,0 +1,14 @@
+// Variables
+// let , var , or const.
+
+var name1 = "Usman";
+let name2 = "Leena";
+const name3 = "Zaki";
+
+var name1 = "Nasir";
+// let name2 = "Sara";
+// const name3 = "Hasan";
+
+console.log (name1);
+console.log (name2);
+console.log (name3);
