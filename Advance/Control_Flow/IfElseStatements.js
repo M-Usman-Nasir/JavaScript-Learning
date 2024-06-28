899bb20ac430f8cb201464843e0e80302e197bdb
Mohammad Usman Nasir <usmannasir98@gmail.com>
2024-06-28 16:30:14 +0500

    Control Flows added

--- Advance/Control_Flow/IfElseStatements.js
+++ Advance/Control_Flow/IfElseStatements.js
@@ -1,8 +1,46 @@
-// If Statements
+// _________ If Statements ________________
 
 // let grade = 60;
 // if (grade < 70) {
 //   console.log ("You failed!");
 // }
 
-// 
+// _________ If and Else & Else If Statements _______
+
+//  Example: 1
+// let age = 31;
+// let minimumAge = 18;
+// if (age >= minimumAge) {
+//   console.log("You are eligible.");
+// } else {
+//   console.log("You are not eligible.");}
+
+// Example: 2
+//   let age = 12;
+//   if (age < 10) {
+//     console.log("Free lemonade for you!");
+//   } else {
+//     console.log("Lemonade costs $1.");
+//   }
+  
+// Example: 3
+// let age = 25;
+// if (age < 10) {
+//   console.log("Free lemonade for you!");
+// } else if (age < 18) {
+//   console.log("Half-price lemonade for teenagers!");
+// } else {
+//   console.log("Lemonade costs $1.");
+// }
+
+// ___________________ For Loop Statements ____________________
+
+// Example: 1
+// for (let i = 1; i <= 5; i++) {
+//   console.log("Iteration", i);
+// }
+
+// Example: 2
+// for (let i = 0; i < 5; i++) {
+//     console.log("Taking order for customer", i + 1);
+//   }
