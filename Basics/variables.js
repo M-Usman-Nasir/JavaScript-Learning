dfb4d7592ea97289d9489e8c928388c0a364a250
Mohammad Usman Nasir <usmannasir98@gmail.com>
2024-06-28 23:29:50 +0500

    Added more lines

--- Basics/variables.js
+++ Basics/variables.js
@@ -1,5 +1,9 @@
 // Variables
 // let , var , or const.
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
 
 var name1 = "Usman";
 let name2 = "Leena";
@@ -12,3 +16,19 @@ var name1 = "Nasir";
 console.log (name1);
 console.log (name2);
 console.log (name3);
+
+const accountId = 8127146
+let accountEmail = "usman@gmail.com"
+var accountPassword = "12345"
+
+accountId = "Karachi"
+accountEmail = "nasir@gmail.com"
+accountPassword = "678910"
+accountId = "Lahore"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountId);
+
+console.table ([accountEmail, accountId, accountPassword, accountId]);
