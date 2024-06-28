620333360ba18693e1a233a23cbebdc250359e8d
Mohammad Usman Nasir <usmannasir98@gmail.com>
2024-06-28 23:38:37 +0500

    Added files

--- Basics/primitiveDataTypes.js
+++ Basics/primitiveDataTypes.js
@@ -1,6 +1,14 @@
 // • JavaScript Syntax and Data Types (Primitives)
 // • Primitive Data Types (Stack Memory)
 
+"use strict"; // treat all JS code as new version 
+
+alert (3 + 3) // we are using node.js, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
 let firstName = "Ali";
 let unassigned;
 let isMarried = true;
---
+++ Basics/stringProperties.js
@@ -0,0 +1,37 @@
+// const myName = "Usman"
+// const repoCount = 50
+
+// console.log(myName + repoCount + " Value");
+
+// console.log(`Hello my name is ${myName} and my repo count is ${repoCount}`);
+
+// const gameName = new String('Usman-Nasir-Badar')
+// const newString = gameName.substring(0, 9)
+// console.log(newString);
+
+// console.log(gameName.indexOf('r'));
+// console.log(gameName.charAt(2));
+// console.log(gameName.toUpperCase());
+// console.log(gameName[0]);
+// console.log(gameName);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+
+// const gameName = new String('Usman-Nasir-Badar')
+// const anotherString = gameName.slice(0, 4)
+// console.log(anotherString);
+
+// const newStringOne = "   Usman    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+
+// console.log(gameName.split('-'));
+
+// const myHouse = "Al-Badar"
+// console.log(myHouse);
