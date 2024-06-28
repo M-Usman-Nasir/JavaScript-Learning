a7ea27475a1ea332c0505d066483ba60a96bc4b7
Mohammad Usman Nasir <usmannasir98@gmail.com>
2024-06-28 12:04:20 +0500

    Some operators added

--- Basics/operators.js
+++ Basics/operators.js
@@ -1,11 +1,51 @@
 // Operators
 // 
 
+// 1. Arithmetic Operators:
 
+// i. Addition
+let a = 10;
+let b = 5;
+let addition = a + b;
+// console.log (addition);
+
+// ii. Subtraction
+let subtraction = a - b;
+// console.log (subtraction);
+
+// iii. Multiplication
+let multiplication = a * b;
+// console.log (multiplication);
+
+// iv. Division
+let division = a / b;
+// console.log (division);
+
+// v. Modulus
+let modulus = a % b;
+// console.log (modulus);
+
+// vi. Increment
+let increment = ++a;
+// console.log (increment);
+
+// vii. Decrement
+let decrement = --b;
+// console.log (decrement);
+
+// 2. Assignment Operators:
+
+let c = 20;
+// c += 10; 		        // c = c + 10;    		// 30
+// c -= 5;  		        // c = c - 5;     		// 25
+// c *= 2;  		        // c = c * 2;     		// 50
+// c /= 5;  		        // c = c / 5;     		// 10
+// c %= 3;  		        // c = c % 3;     		// 1
+// c **= 2; 		        // c = c ** 2;    		// 1
+// console.log (c);
 
 // ● Exponentiation Example:
-// let num1 = 2;		let num2 = 2;
-// console.log (num1 ** num2);
-
-
-
+let base = 2;
+let exponent = 3;
+let result = base ** exponent;
+// console.log (result);
